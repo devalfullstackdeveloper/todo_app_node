7f2190b336d244be7daa889faf19ae27c45bcf5f
dhruvikpatel12 <125969219+dhruvikpatel12@users.noreply.github.com>
2023-05-02 13:02:33 +0530

    all add and delete leads

---
+++ controller/lead.controller.js
@@ -0,0 +1,184 @@
+const { compareSync } = require("bcrypt");
+const commonService = require("../services/common.services");
+
+const showlead = async (req, res) => {
+    try {
+      let emp_code = req. params.emp_code;
+      let tblName = "tbl_lead";
+      let parameters = "*"
+      let condition  = `flag= 0 ORDER BY id desc`;
+      let queryResult = await commonService.sqlSelectQueryWithParametrs(
+        tblName,
+        parameters,
+        condition
+      );
+      if (queryResult.success) {
+        res.status(200).send({
+          status: 200,
+          data: queryResult.result,
+        });
+      } else {
+        res.status(500).send({
+          status: 500,
+          message: "Something went wrong!",
+          error: queryResult.error,
+        });
+      }
+    } catch (e) {
+      res.status(500).send({
+        status: 500,
+        message: "Something went wrong!",
+        error: e,
+      });
+    }
+  };
+
+
+//Adding lead
+const leadAdd = async (req, res) => {
+  try {
+    let payload = req.body;
+
+    let todayDate = new Date()
+    let todayDate1= todayDate.toISOString().split('T')[0]
+
+    let parameters = {   
+      first_name:payload.first_name,
+      last_name:payload.last_name,
+      company: payload.company,
+      title:payload.title,
+      lead_status:payload.lead_status,
+      phone_number:payload.phone_number,
+      email:payload.email,
+      priority:payload.priority,
+      street:payload.street,
+      city:payload.city,
+      zipcode:payload.zipcode,
+      website:payload.website,
+      no_of_employee:payload.no_of_employee,
+      lead_source:payload.lead_source,
+      annual_revenue:payload.annual_revenue,
+      industry:payload.industry
+      
+    };
+
+    let tblName = "tbl_lead";
+  
+    let parameters1 = "Count(id) As count";
+
+    let condition1 = " email='" +
+    payload.email +
+    "' AND flag='" +
+    0 +
+          "' ";
+      
+      let queryResult1 = await commonService.sqlSelectQueryWithParametrs(
+          tblName,
+          parameters1,
+          condition1
+      );
+     
+      if (queryResult1.success) {
+       
+        if(queryResult1.result[0].count==0)
+        {
+
+          let queryResult = await commonService.sqlQueryWithParametrs(
+            tblName,
+            parameters
+          );
+      
+          if (queryResult.success) {
+            res.status(200).send({
+              status: 200,
+              message: "lead successfully Added",
+            });
+          } else {
+            res.status(500).send({
+              status: 500,
+              message: "Something went wrong1!",
+              error: e,
+            });
+          }
+      
+
+        }
+        else{
+          res.status(403).send({
+            status: 403,
+            message: "Data Already Present!",
+          });
+        }
+   
+      } else {
+        res.status(500).send({
+          status: 500,
+          message: "Something went wrong!",
+          error: e,
+        });
+      } 
+  } catch (e) {
+    res.status(500).send({
+      status: 500,
+      message: "Something went wrong!",
+      error: e,
+    });
+  }
+ 
+};
+
+//Deleting lead
+const leadDelete = async (req, res) => {
+  try {
+   
+    let id = req.params.id;
+   
+    let parameters =
+      "flag = 1"+
+      " Where id = " +
+      id +
+      "";
+
+
+    let tblName = "tbl_lead";
+ 
+      let queryResult = await commonService.sqlUpdateQueryWithParametrs(
+        tblName,
+        parameters
+      );
+      if (queryResult.success) {
+        res.status(200).send({
+          status: 200,
+          message: "Deleted Record Successfully",
+        });
+      } else {
+        res.status(500).send({
+          status: 500,
+          message: "Something went wrong2!",
+          error: queryResult.error,
+        });
+      }
+
+  } catch (e) {
+    res.status(500).send({
+      status: 500,
+      message: "Something went wrong1!",
+      error: e,
+    });
+  }
+};
+
+
+
+
+
+
+
+
+
+module.exports = {
+  
+ showlead,
+ leadAdd,
+ leadDelete
+};
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authRoutes = require('./auth.route');
+const leadRoutes = require('./lead.route');
+
 
 const { verifyToken } = require('../middleware/verifyToken');
 
 
 const router = express.Router();
 router.use('/auth', authRoutes);
-
+router.use('/lead', leadRoutes);
 
 module.exports = router;
