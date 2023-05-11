a5a63edeecf0f8bf9c8334664dc3db559433236a
Chaitanya1312000 <126552476+Chaitanya1312000@users.noreply.github.com>
2023-05-11 14:58:03 +0530

    changes made attachments module

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post('/add_attachments', uploadController.checkDir, uploadController.uploadDocument, uploadController.addDocument);
 router.get('/get_attachments', uploadController.getDocument);
-router.get('/delete_attachments/:id', uploadController.deleteDocument);
+router.post('/delete_attachments/:id', uploadController.deleteDocument);
 
 module.exports = router;
